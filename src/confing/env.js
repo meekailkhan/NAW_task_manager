258e33b628f3113af08c6ed06fb96b26ba0dc37d
Meekail <meekail@meekail.localdomain>
2026-02-14 07:00:45 +0530

    change port

--- src/confing/env.js
+++ src/confing/env.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const env = {
-    PORT : process.env.PORT || 3000,
+    PORT : process.env.PORT || 5000,
     MONGO_URI : process.env.MONGO_URI,
     JWT_SECRET : process.env.JWT_SECRET || "secret"
 }
